14fd6f3218c1fce691484baed3c15100354dc7c0
H51M14\Student <Student@gmail.com>
2025-08-21 08:50:48 +0700

    bai 4

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Person } from "./bai1";
 import { Student } from './bai2';
 import { Car } from './bai3';
+import { Rectangle } from './bai4';
 
 var person = new Person('Duy',22)
 person.displayInfo();
@@ -10,3 +11,9 @@ student1.displayAllInfo();
 
 const car1 = new Car("Toyota", "Camry", 2022);
 car1.showInfo();
+
+
+const rect1 = new Rectangle(5, 10);
+
+console.log("Area:", rect1.getArea());          
+console.log("Perimeter:", rect1.getPerimeter()); 
