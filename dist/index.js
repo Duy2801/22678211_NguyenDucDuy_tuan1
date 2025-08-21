fcd40af9eb0a8534d089d4e19d5d97a77039b55b
H51M14\Student <Student@gmail.com>
2025-08-21 08:43:17 +0700

    bai 2

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const bai1_1 = require("./bai1");
+const bai2_1 = require("./bai2");
 var person = new bai1_1.Person('Duy', 22);
 person.displayInfo();
+const student1 = new bai2_1.Student("Duy", 22, "A");
+student1.displayAllInfo();
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { Person } from "./bai1";
+import { Student } from './bai2';
 
 var person = new Person('Duy',22)
 person.displayInfo();
+
+const student1 = new Student("Duy", 22, "A");
+student1.displayAllInfo();
