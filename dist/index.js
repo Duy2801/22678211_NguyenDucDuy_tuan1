11ca77f9238b76e6f7ff3c9e8b7d5eb774e048d3
Duy2801 <nguyenducduy544645@gmail.com>
2025-08-24 12:53:14 +0700

    bai 8

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,7 @@ const bai4_1 = require("./bai4");
 const bai5_1 = require("./bai5");
 const bai6_1 = require("./bai6");
 const bai7_1 = require("./bai7");
+const bai8_1 = require("./bai8");
 var person = new bai1_1.Person("Duy", 22);
 person.displayInfo();
 const student1 = new bai2_1.Student("Duy", 22, "A");
@@ -25,3 +26,13 @@ const book = new bai6_1.Book("5 anh em sieu nhan", "hung", 2021);
 book.showBook();
 const user = new bai7_1.User("Duy");
 console.log(user.name);
+const products = [
+    new bai8_1.Product("Book", 50),
+    new bai8_1.Product("Laptop", 1200),
+    new bai8_1.Product("Phone", 800),
+    new bai8_1.Product("Pen", 20),
+    new bai8_1.Product("Headphones", 150),
+];
+const expensiveProducts = products.filter((p) => p.price > 100);
+console.log("Products with price > 100:");
+expensiveProducts.forEach((p) => console.log(`${p.name} - $${p.price}`));
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { Rectangle } from "./bai4";
 import { BankAccount } from "./bai5";
 import { Book } from "./bai6";
 import { User } from "./bai7";
+import { Product } from "./bai8";
 var person = new Person("Duy", 22);
 person.displayInfo();
 
@@ -31,3 +32,16 @@ book.showBook();
 
 const user = new User("Duy");
 console.log(user.name);
+
+const products: Product[] = [
+  new Product("Book", 50),
+  new Product("Laptop", 1200),
+  new Product("Phone", 800),
+  new Product("Pen", 20),
+  new Product("Headphones", 150),
+];
+
+const expensiveProducts = products.filter((p) => p.price > 100);
+
+console.log("Products with price > 100:");
+expensiveProducts.forEach((p) => console.log(`${p.name} - $${p.price}`));
